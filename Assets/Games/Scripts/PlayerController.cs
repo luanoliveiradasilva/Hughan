d709c3740db3774815c0d8d266391b15e2ce4015
Luan Nascimento Oliveira <luanoliveira1107@gmail.com>
2023-05-21 13:07:30 +0100

    Atualização da movimentação do personagem.
    Adicionado movimentação mais suave para o personagem.

--- Assets/Games/Scripts/PlayerController.cs
+++ Assets/Games/Scripts/PlayerController.cs
@@ -5,53 +5,138 @@ namespace Assets.Unity___Foundations_of_Audio.Scripts.System
     public class PlayerController : MonoBehaviour
     {
 
+
+        [Header("Player")]
+        [Tooltip("Move Speed Player")]
+        public float moveSpeedPlayer = 4.0f;
+        [Tooltip("Sprint speed of the character")]
+        public float SprintSpeed = 6.0f;
+        [Tooltip("Rotation speed of the character")]
+        public float RotationSpeed = 1.0f;
+        [Tooltip("Acceleration and deceleration")]
+        public float SpeedChangeRate = 10.0f;
+        [Range(0.0f, 0.7f)]
+        public float RotationSmoothTime = 0.12f;
+
+        [Header("Cinemachine")]
+        public GameObject CinemachineCameraTarget;
+        [Tooltip("Move camera up")]
+        public float TopClamp = 90.0f;
+        [Tooltip("Move camera Down")]
+        public float BottomClamp = -90.0f;
+
+        private int _animationPlayerHash = Animator.StringToHash("Input");
+
         private CharacterController characterController;
         private Animator animator;
         private Vector3 playerCharacterMove;
+        private Vector3 moveCamera;
+        private GameObject _mainCamera;
+        private Rigidbody _rigidbody;
 
+        private float _targetRotation = 0.0f;
+        private float _rotationVelocity;
+        private float _verticalVelocity;
+        private float _threshold = 0.01f;
+        private float _cinemachineTargetPitch;
 
-        private float moveSpeedPlayer = 0.5f;
-        float moveSpeedCamera = 50.0f;
-        private int inputXHas = Animator.StringToHash("inputX");
-        private int inputYHas = Animator.StringToHash("inputY");
+        private void Awake()
+        {
+            // get a reference to our main camera
+            if (_mainCamera == null)
+            {
+                _mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+            }
+        }
 
         private void Start()
         {
             characterController = GetComponent<CharacterController>();
             animator = GetComponent<Animator>();
+            _rigidbody = GetComponent<Rigidbody>();
         }
 
         private void Update()
         {
-
-            float moveHorizontal = Input.GetAxis("L_Horizontal");
-            float moveVertical = Input.GetAxis("L_Vertical");
-
-            playerCharacterMove = new Vector3(moveHorizontal, moveVertical);
-
-            if (Mathf.Abs(moveHorizontal) > 0.01f || Mathf.Abs(moveVertical) > 0.01f)
-            {
-                animator.SetFloat(inputXHas, playerCharacterMove.x);
-                animator.SetFloat(inputYHas, playerCharacterMove.y);
-
-                Vector3 movement = new Vector3(moveHorizontal, -9.81f, moveVertical);
-
-                characterController.Move(movement * moveSpeedPlayer * Time.deltaTime);
-            }
-
-            MovePlayerVertical();
-
+            MovePlayer();
+            MoveCameraRotation();
         }
 
-        private void MovePlayerVertical()
+        private void MovePlayer()
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+
+            Debug.Log("Horizontal" + moveHorizontal);
+            Debug.Log("Vertical" + moveVertical);
+
+            playerCharacterMove = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+            if (playerCharacterMove != Vector3.zero)
+            {
+                if (moveHorizontal != 0.0f || moveVertical != 0.0f)
+                {
+                    float moveSpeedMultiplier = moveHorizontal > 0.0f || moveVertical > 0.0f ? 1.0f : 3.0f;
+                    animator.SetFloat(_animationPlayerHash, playerCharacterMove.sqrMagnitude + moveSpeedMultiplier);
+                    _rigidbody.velocity = transform.forward * moveSpeedPlayer * moveSpeedMultiplier;
+                }
+                else if (moveHorizontal < 0.0f || moveVertical < 0.0f)
+                {
+                    animator.SetFloat(_animationPlayerHash, playerCharacterMove.sqrMagnitude + 3);
+                    _rigidbody.velocity = -transform.forward * moveSpeedPlayer;
+                }
+
+                //Input  directions of character with base in rotation and used calculate of Euler
+                Vector3 inputDirection = new Vector3(playerCharacterMove.x, 0.0f, playerCharacterMove.y).normalized;
+
+                _targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg + _mainCamera.transform.eulerAngles.y;
+                float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity, RotationSmoothTime);
+                transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+
+                Vector3 targetDirection = Quaternion.Euler(0.0f, _targetRotation, 0.0f) * Vector3.forward;
+
+                characterController.Move(targetDirection.normalized * (moveSpeedPlayer * Time.deltaTime) +
+                             new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
+            }
+            else
+            {
+                animator.SetFloat(_animationPlayerHash, 0);
+            }
+        }
+
+        private void MoveCameraRotation()
         {
 
-            // Obtém a posição do analógico direito (R3)
-            float moveRVertical = Input.GetAxis("R_Vertical");
+            float moveRVertical = Input.GetAxis("Vertical");
+            float moveRHorizontal = Input.GetAxis("Horizontal");
 
-            Vector3 movementCamera = new Vector3(0.0f, moveRVertical, 0.0f);
+            Debug.Log("Rotation" + moveCamera.magnitude);
 
-            transform.Rotate(movementCamera * moveSpeedCamera * Time.deltaTime);
+            moveCamera = new Vector3(moveRHorizontal, 0.0f, moveRVertical);
+
+            if (moveCamera.magnitude >= _threshold)
+            {
+                float deltaTimeMultiplier = moveRHorizontal > 0.0f || moveRVertical > 0 ? 1.0f : Time.deltaTime;
+
+                _cinemachineTargetPitch += moveCamera.y * RotationSpeed * deltaTimeMultiplier;
+                _rotationVelocity = moveCamera.x * RotationSpeed * deltaTimeMultiplier;
+
+                // clamp our pitch rotation
+                _cinemachineTargetPitch = ClampAngle(_cinemachineTargetPitch, BottomClamp, TopClamp);
+
+                // Update Cinemachine camera target pitch
+                CinemachineCameraTarget.transform.localRotation = Quaternion.Euler(_cinemachineTargetPitch, 0.0f, 0.0f);
+
+                // rotate the player left and right
+                transform.Rotate(Vector3.up * _rotationVelocity);
+            }
+        }
+
+        private static float ClampAngle(float lfAngle, float lfMin, float lfMax)
+        {
+            if (lfAngle < -360f) lfAngle += 360f;
+            if (lfAngle > 360f) lfAngle -= 360f;
+            return Mathf.Clamp(lfAngle, lfMin, lfMax);
         }
 
     }
